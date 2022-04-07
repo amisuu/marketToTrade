90f5644d2c349b7292dcb00107f049b4db37a6d0
amisuu <liver13@wp.pl>
2022-04-07 12:43:33 +0200

    Dodaj pliki projektów.

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
